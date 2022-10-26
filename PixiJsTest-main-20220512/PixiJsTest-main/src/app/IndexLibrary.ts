7789b420b271adc20cc39ce90b1475e9954098be
dudududu-123 <116569715+dudududu-123@users.noreply.github.com>
2022-10-26 22:04:35 +0900

    222

---
+++ PixiJsTest-main-20220512/PixiJsTest-main/src/app/IndexLibrary.ts
@@ -0,0 +1,54 @@
+//このクラスがおおもとのライブラリで、これをqualtricsまたはindex.htmlで呼び出して使っている。
+
+import "../css/index.scss";
+import * as bootstrap from "bootstrap";
+import { App } from "./App";
+import { ModelPosition } from "./types";
+
+export default class IndexLibrary {
+    private debug: boolean;
+    private serverURL: string;
+    private modelPath: string;
+    private modelPosition: ModelPosition;
+    private app: App | null;
+    private agent_point: number =0;
+    private limit: number =0;
+
+
+    constructor(debug: boolean, serverURL: string, modelPath: string, modelPosition: ModelPosition) {
+        this.debug = debug;
+        this.serverURL = serverURL;
+        this.modelPosition = modelPosition;
+        this.app = null;
+        this.modelPath = modelPath;
+    }
+    onload = () => {
+        console.log("DOMLoadedAction");
+        this.app = new App(this.debug, this.serverURL, this.modelPath, this.modelPosition); //
+        console.log("ロードした");
+        this.app.mount();
+    };
+    onUnload = () => {
+        console.log("WindowLoadedAction");
+        console.log("アンロード");
+        this.app?.unmount();
+    };
+
+    set_agentpoint = (point: number) => {
+        this.agent_point = point;
+    }
+    set_limit = (limit: number) => {
+        this.limit = limit;
+    }
+
+    App_set_point =() => {
+        this.app?.change_face(this.agent_point, this.limit);
+    }
+}
+
+//http://localhost:40080
+//http://192.168.3.10:40080
+
+//550, 900, 0.235, 0, -20 モデル全身/
+//550, 700, 0.45, 0, 500 モデル顔中心
+//225, 350, 0.25, 0, 250
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # AgentInteractionResources
+「PixiJsTest-main-20220512.zip」がNodeプロジェクトフォルダです。
+
+# 展開したプロジェクトの実行方法
+1.node環境を作る https://docs.microsoft.com/ja-jp/windows/dev-environment/javascript/nodejs-on-windows
+2.プロジェクトのルートフォルダー（package.jsonがあるフォルダ階層）にcmd等で移動する。
+3.「npm install」とcmdで入力(npmパッケージインストール)
+4.「npm run build-dev」(ビルドしてdistフォルダ生成)
+5.「npm run start-dev」(ブラウザで実行)
